a533b327739c5019178296d2f8445c251cf01783
OmarZahrah <omar.zahrah01@gmail.com>
2026-04-07 15:40:09 +0200

    Refactor server.js to simplify server startup; update connectDB function in db.js for direct connection handling

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 require("dotenv").config();
 const connectDB = require("./config/db");
 const app = require("./app");
+connectDB();
 
 const PORT = process.env.PORT || 3000;
-
-connectDB()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Database connection failed:", error.message);
-    process.exit(1);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+// connectDB()
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("Database connection failed:", error.message);
+//     process.exit(1);
+//   });
